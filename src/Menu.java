c0faaca84412d31c855252d28755107f6322dc94
Travis Boyden <43623170+travib11@users.noreply.github.com>
2019-10-19 16:38:50 +0800

    Update Menu.java

--- src/Menu.java
+++ src/Menu.java
@@ -6,7 +6,7 @@ public class Menu {
 	//Used to provide CustomerInterface with the currently available menu items
 	public Item[] getItems() {
 		Item[] items = new Item[menuItems.size()];
-		for(int i = 0; i < menuItems.size() - 1 ; i++) {
+		for(int i = 0; i < menuItems.size(); i++) {
 			items[i] = menuItems.get(i);
 		}
 		return items;
