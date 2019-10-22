e0ddf665c5a193dd8cda29d8d3975de9b0bf993c
veronicakkv <55283210+veronicakkv@users.noreply.github.com>
2019-10-22 21:08:12 +0800

    Add files via upload

---
+++ src/src/testCMS/userTests.java
@@ -0,0 +1,39 @@
+package testCMS;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.lang.reflect.*;
+import java.util.ArrayList;
+
+import canteen.*;
+public class userTests {
+	@Test
+	public void user_getbalance1() {
+		User u1 = new User("George",1111,50.0);
+		double balance = u1.getBalance();
+		assertEquals(50,balance,0.001);
+	}
+	@Test
+	public void user_set_balance() {
+		User u2 = new User("Margie",1112,10.0);
+		//double balance = u1.setBalance(20);
+		u2.setBalance(20);
+		double balance2 = u2.getBalance();
+		assertEquals(20,balance2,0.0001);
+	}
+	@Test
+	public void user_add_to_balance() {
+		User u3 = new User("Rick",1113,50.0);
+		//double balance = u1.setBalance(20);
+		u3.addToBalance(20);
+		double balance = u3.getBalance();
+		assertEquals(70,balance,0.0001);
+	}
+	@Test
+	public void user_remove_from_balance() {
+		User u4 = new User("Morty",1114,20.34);
+		//double balance = u1.setBalance(20);
+		u4.removeFromBalance(3);
+		double balance = u4.getBalance();
+		assertEquals(17.34,balance,0.0001);
+	}
+}
