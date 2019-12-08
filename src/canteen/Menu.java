b17d63d4d384321aa3bdf52d27a4b05d07ebac88
Travis Boyden <43623170+travib11@users.noreply.github.com>
2019-12-08 13:45:30 +0800

    Update Menu.java

--- src/canteen/Menu.java
+++ src/canteen/Menu.java
@@ -3,7 +3,10 @@ package canteen;
 import java.util.ArrayList;
 
 public class Menu {
+	//Catalog of Menue Items
 	private ArrayList<Item> menuItems;
+	//List of all Interfaces dependent on the Menu
+	private ArrayList<Observer> observers;
 	
 	public Menu(ArrayList<Item> m) {
 		this.menuItems = m;
@@ -20,9 +23,27 @@ public class Menu {
 	
 	public void addItem(Item i) {
 		this.menuItems.add(i);
+		updateObservers();
+
 	}
 	
 	public void removeItem(Item i) {
 		this.menuItems.remove(i);
+		updateObservers();
+	}
+	
+	
+	public void updateObservers(){
+		for(Observer o: observers) {
+			o.update(this);
+		}
+	}
+	
+	public void addObserver(Observer o) {
+		observers.add(o);
+	}
+	
+	public void removeOberver(Observer o) {
+		observers.remove(o);
 	}
 }
